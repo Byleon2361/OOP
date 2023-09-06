3a188effdb90d654266523f0a640139b9dc29295
Boyvalenko Nikita <n.boywalenko@yandex.ru>
2023-09-06 18:51:02 +0700

    Complete 2 part lab

--- lab1/two-dimensional.c
+++ lab1/two-dimensional.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-int *genRandMatrix(int size, int maxValue)
+int **genRandMatrix(int size, int maxValue)
 {
+    int **matrix = malloc(sizeof(int *) * size);
+    // int *newArr = malloc(sizeof(int) * (size + 1));
+    int sizeNewArr;
+    for (int i = 0; i < size; i++)
+    {
+        sizeNewArr = rand() % 10;
+        matrix[i] = malloc(sizeof(int) * (sizeNewArr + 1));
+        matrix[i][0] = sizeNewArr;
+        for (int j = 1; j < sizeNewArr + 1; j++)
+        {
+            matrix[i][j] = rand() % maxValue;
+        }
+    }
+    return matrix;
 }
-void printMatrix(int *arr)
+void printMatrix(int **arr, int size)
 {
+    for (int i = 0; i < size; i++)
+    {
+        int size = arr[i][0];
+        printf("%d: ", size);
+        for (int j = 1; j < size; j++)
+        {
+            printf("%d, ", arr[i][j]);
+        }
+        printf("%d\n", arr[i][size]);
+    }
 }
 int main()
 {
     srand(time(NULL));
     int size = rand() % 10;
     int maxValue = 100;
-    int **matrix = genRandArray(size, maxValue);
-    print(matrix);
+    int **matrix = genRandMatrix(size, maxValue);
+    printf("Size matrix: %d\n", size);
+    printMatrix(matrix, size);
     free(matrix);
 }
