e6929687f6d4bc6ea486d4929a817f808c07f6b3
Boyvalenko Nikita <n.boywalenko@yandex.ru>
2023-09-12 21:41:26 +0700

    Add SpiralFromCenter function

--- lab2/lab2_1.c
+++ lab2/lab2_1.c
@@ -24,6 +24,7 @@ void printMatrix(int **arr, int n)
         }
         printf("%d\n", arr[i][n - 1]);
     }
+    printf("---------------------------\n");
 }
 int *SpiralFromLeftAngle(int **matrix, int n)
 {
@@ -63,6 +64,48 @@ int *SpiralFromLeftAngle(int **matrix, int n)
     }
     return newArr;
 }
+int *SpiralFromCenter(int **matrix, int n)
+{
+    int size = n * n;
+    int *newArr = malloc(sizeof(int) * size);
+
+    int center;
+    if (n % 2 == 0)
+        center = n / 2 - 1;
+    else
+        center = n / 2;
+    int DownBorder = center + 1;
+    int RightBorder = center + 1;
+    int UpBorder = center - 1;
+    int LeftBorder = center - 1;
+
+    int i = center; // Координары строки
+    int j = center; // Координаты столбца
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int m = 0; m < n; m++)
+        {
+            newArr[i * n + j] = matrix[k][m];
+            if (i == UpBorder + 1 && j < RightBorder)
+                j++;
+            else if (j == RightBorder && i < DownBorder)
+                i++;
+            else if (i == DownBorder && j > LeftBorder)
+                j--;
+            else if (j >= LeftBorder && i > UpBorder)
+                i--;
+            if ((i - 1 == UpBorder) && (j == LeftBorder)) // прошел круг, нужно увеличить рамки
+            {
+                DownBorder++;
+                RightBorder++;
+                UpBorder--;
+                LeftBorder--;
+            }
+        }
+    }
+    return newArr;
+}
 void printArr(int *arr, int n)
 {
     for (int i = 0; i < n; i++)
@@ -73,24 +116,33 @@ void printArr(int *arr, int n)
         }
         printf("\n");
     }
+    printf("---------------------------\n");
 }
 int main()
 {
+
     srand(time(NULL));
     int n = 0;
     int maxValue = 100;
+
     puts("Введите размер квадратной матрицы");
     scanf("%d", &n);
     int **matrix = genRandMatrix(n, maxValue);
+
     printf("Size matrix: %d\n", n);
     printMatrix(matrix, n);
 
-    printf("---------------------------\n");
-    int *arr = SpiralFromLeftAngle(matrix, n);
-    printArr(arr, n);
+    puts("SpiralFromLeftAngle\n");
+    int *arr1 = SpiralFromLeftAngle(matrix, n);
+    printArr(arr1, n);
+
+    puts("SpiralFromCenter\n");
+    int *arr2 = SpiralFromCenter(matrix, n);
+    printArr(arr2, n);
 
     for (int i = 0; i < n; i++)
         free(matrix[i]);
     free(matrix);
-    free(arr);
+    free(arr1);
+    free(arr2);
 }
