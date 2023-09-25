33c4df7c48fdc919c634ebc9e4477efae7ba1619
azard285 <pavlenkopasa382@gmail.com>
2023-09-26 02:46:16 +0700

    anethink

--- lab3/src/SourceFiles/Rabbit.cpp
+++ lab3/src/SourceFiles/Rabbit.cpp
@@ -1,15 +1,63 @@
 #include "Rabbit.h"
-#include "Fox.h"
 #include <iostream>
+#include <cstdlib>
+using namespace std;
+Rabbit::Rabbit()
+{
+    deathAge = 10;
+    step = 1;
+    age = 0;
+    stability = 0;
+    x = 0;
+    y = 0;
+    direction = rand() % 3;
+}
+void Rabbit::change(int d)
+{
+    switch(direction)
+    {
+    case 0:
+        y += step;q
+        break;
+    case 1:
+        x += step;
+        break;
+    case 2:
+        y -= step;
+        break;
+    case 3:
+        x -= step;
+        break;
+    }
+}
+void Rabbit::changeDirection()
+{
+    if ((rand()%100) < (100 - stability))
+    {
+        direction++;
+        if(direction == 3)
+            direction = 0;
+    }
+}
 int Rabbit::changeAge()
 {
+    if (age > 10)
+    {
+        sex();
+        this->~Rabbit();
+    }
+    return age++;
 }
+/*//нахуй
 void Rabbit::eat()
 {
 }
+*/
 void Rabbit::death()
 {
+    this->~Rabbit();
 }
-Animal *Rabbit::sex()
+void Rabbit::sex()
 {
+
 }
