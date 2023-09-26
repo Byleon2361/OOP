06fb4e931d1edfbd3037a3a65cf8f81f934c3378
azard285 <pavlenkopasa382@gmail.com>
2023-09-26 21:58:10 +0700

    fucking rebase add all changes in main

--- lab3/src/HeaderFiles/Rabbit.h
+++ lab3/src/HeaderFiles/Rabbit.h
@@ -1,18 +1,29 @@
 #pragma once
 #include "Animal.h"
-class Rabbit : public Animal
+class Rabbit : protected Animal
 {
 public:
     Rabbit() : Animal()
     {
         Animal::deathAge = 10;
-        Animal::step = 1;
+        Animal:: step = 1;
     }
-    ~Rabbit() {}
+    Rabbit(int x, int y) : Rabbit()
+    {
+        this->x = x;
+        this->y = y;
+    }
+    ~Rabbit() {};
+    void Move();
 
 private:
-    int changeAge() override;
-    void eat();             // 🍍
-    void death() override;  // ☠
-    Animal *sex() override; // 🥵
+    int changeAge() ;
+    void death() ; // ☠
+    Animal *sex() ;   // 🥵
+
+public:
+    int get_x() const {return x;}
+    int get_y() const {return y;}
+    int get_stability() const {return stability;}
+    int get_direction() const {return direction;}
 };
--- lab3/src/SourceFiles/Rabbit.cpp
+++ lab3/src/SourceFiles/Rabbit.cpp
@@ -2,43 +2,12 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-Rabbit::Rabbit()
+
+viod Fox::Move()
 {
-    deathAge = 10;
-    step = 1;
-    age = 0;
-    stability = 0;
-    x = 0;
-    y = 0;
-    direction = rand() % 3;
+    change(changeDirection());
 }
-void Rabbit::change(int d)
-{
-    switch(direction)
-    {
-    case 0:
-        y += step;q
-        break;
-    case 1:
-        x += step;
-        break;
-    case 2:
-        y -= step;
-        break;
-    case 3:
-        x -= step;
-        break;
-    }
-}
-void Rabbit::changeDirection()
-{
-    if ((rand()%100) < (100 - stability))
-    {
-        direction++;
-        if(direction == 3)
-            direction = 0;
-    }
-}
+
 int Rabbit::changeAge()
 {
     if (age > 10)
@@ -46,18 +15,17 @@ int Rabbit::changeAge()
         sex();
         this->~Rabbit();
     }
-    return age++;
+    
+    eturn age++;
 }
-/*//нахуй
-void Rabbit::eat()
-{
-}
-*/
+
 void Rabbit::death()
 {
     this->~Rabbit();
 }
+
 void Rabbit::sex()
 {
-
+    Rabbit *newRabbit = this;
+    return newRabbit;
 }
